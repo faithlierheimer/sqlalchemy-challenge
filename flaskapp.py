a5efb7c141fdb7e682e393674fec87cfb3ba8d7e
faithlierheimer <flierheimer@gmail.com>
2020-08-23 21:03:54 -0600

    I THINK THE FLASK APP IS WORKING. I AM GOING TO BED NOW.

--- flaskapp.py
+++ flaskapp.py
@@ -78,7 +78,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     print("Server received request for homepage.")
-    return "Available routes:<br/>/api/v1.0/precipitation: Returns JSON version of precipitation data over last 12 months.<br/> /api/v1.0/stations: Returns JSON version of stations in dataset. <br/> /api/v1.0/tobs: Returns JSON list of temperature observations for the previous year. <br/> /api/v1.0/<start>` and `/api/v1.0/<start>/<end> Returns JSON list of min, avg, and max temp for a given start or start-end range. If start only, calculates min, avg, and max for all dates greater than and equal to the start date. When given start and end date, calculates min, avg, and max for dates between the start and end date inclusive. Dates MUST be in following format YYYY-MM-DD."
+    return "Available routes:<br/>/api/v1.0/precipitation: Returns JSON version of precipitation data over last 12 months.<br/> /api/v1.0/stations: Returns JSON version of stations in dataset. <br/> /api/v1.0/tobs: Returns JSON list of temperature observations for the previous year. <br/> /api/v1.0/start` and `/api/v1.0/start/end Returns JSON list of min, avg, and max temp for a given start or start-end range. If start only, calculates min, avg, and max for all dates greater than and equal to the start date. When given start and end date, calculates min, avg, and max for dates between the start and end date inclusive. Dates MUST be in following format YYYYMMDD."
 ##This endpoint works as far as I can tell, as of 8/23. 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
@@ -99,14 +99,15 @@ def temperature():
 #     print("Server received request for dates page")
 #     return jsonify(##min, avg, max temp of given dates##)
 #@app.route("/api/v1.0/<start>")
-@app.route("/api/v1.0/test")
-def datesb():
+@app.route("/api/v1.0/start")
+def datesa():
     return """
         <html><body>
             <h2>Thanks for choosing the date request page!</h2>
             <form action="/date">
                 What start date do you want to check the temperature for?<br>
-                Your answer must be in the YYYY-MM-DD format.<br>
+                Your answer must be in the YYYYMMDD format.<br>
+                You will receive the min, max, and avg temp for everything including and after that date.<br>
                 <input type = 'text' name = 'startdate'><br>
                 <input type = 'submit' value = 'Continue'>
             </form>
@@ -115,30 +116,51 @@ def datesb():
 @app.route("/date")
 def temp_getter():
     startdate = request.args['startdate']
+    start = pd.to_datetime(str(startdate), format = '%Y%m%d').date()
     engine = create_engine("sqlite:///Resources/hawaii.sqlite")
     Base = automap_base()
     Base.prepare(engine, reflect = True)
     Tables = Base.classes.keys()
     measurement = Base.classes.measurement
     station = Base.classes.station
-    temps2_list = []
     session = Session(engine)
-    ###Somehow this is querying the wrong date and querying the same one no matter what i put in. lol. 
-    temps2 = session.query(measurement.date, func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).filter(measurement.date >= str(startdate)).all()
+    temps2 = session.query(measurement.date, func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).group_by(measurement.date).filter(measurement.date >= start).all()
     temps2_df = pd.DataFrame(temps2, columns = ['date', 'min_temp', 'max_temp', 'avg_temp'])
     temps2_dict = temps2_df.to_dict('records')
     return jsonify(temps2_dict)
-    # print("Server received request for start date only page.")
-    # start = request.args['start date']
-    # end = request.args['end date']
-    # if (start != None) and (end != None):
-    #     temps2 = session.query(measurement.date, func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
-    #         filter(measurement.date >= start).filter(measurement.date <= end).all()
-    # elif end != None:
-    #     temps2 = session.query(measurement.date, func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
-    #     filter(measurement.date >= start).all()
-    # temps_df2 = pd.DataFrame(temps2, columns = ['date', 'min_temp', 'max_temp', 'avg_temp'])
-    # temps_dict2 = temps_df2.to_dict('records')
-    # return jsonify(temps_dict2)
+@app.route("/api/v1.0/start/end")
+def datesb():
+    return """
+        <html><body>
+            <h2>Thanks for choosing the date request page!</h2>
+            <form action="/date2">
+                What start and end date do you want to check the temperature in between?<br>
+                Your answers must be in the YYYYMMDD format.<br>
+                You will receive the min, max, and avg temp for everything including and after that date.<br>
+                Your start date must go in the first box, and your end date must go in the second box. <br>
+                <input type = 'text' name = 'startdate'><br>
+                <input type = 'text' name = 'enddate'><br>
+                <input type = 'submit' value = 'Continue'>
+            </form>
+        </body></html>
+        """
+@app.route("/date2")
+def temp_getter2():
+    startdate = request.args['startdate']
+    enddate = request.args['enddate']
+    start = pd.to_datetime(str(startdate), format = '%Y%m%d').date()
+    end = pd.to_datetime(str(enddate), format = '%Y%m%d').date()
+    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+    Base = automap_base()
+    Base.prepare(engine, reflect = True)
+    Tables = Base.classes.keys()
+    measurement = Base.classes.measurement
+    station = Base.classes.station
+    session = Session(engine)
+    temp_range = session.query(measurement.date, func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).group_by(measurement.date).filter(measurement.date >= start).filter(measurement.date <= end).all()
+    temp_range_df = pd.DataFrame(temp_range, columns = ['date', 'min_temp', 'max_temp', 'avg_temp'])
+    temp_range_dict = temp_range_df.to_dict('records')
+    return jsonify(temp_range_dict)
 if __name__ == "__main__":
     app.run(debug = True)
+    
